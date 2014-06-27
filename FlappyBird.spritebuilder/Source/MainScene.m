fdeace6d30c4d4e8cf7dd81ec6f3daf6d7644bfd
lokada <lyokada@ucsc.edu>
2014-06-27 13:43:02 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -65,9 +65,9 @@
     _parallaxBackground = [CCParallaxNode node];
     [_parallaxContainer addChild:_parallaxBackground];
     
-    //Note that the bush ratio is larger than the cloud
-    _bushParallaxRatio = ccp(0.9,1);
-    _cloudParallaxRatio = ccp(0.5,1);
+    // Note that the bush ratio is larger than the cloud
+    _bushParallaxRatio = ccp(0.9, 1);
+    _cloudParallaxRatio = ccp(0.5, 1);
     
     for (CCNode *bush in _bushes) {
         CGPoint offset = bush.position;
